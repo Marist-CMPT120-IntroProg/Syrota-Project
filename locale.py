b6bb7fa9e1a41e60b00af4dd01537cdf7a408b7f
KarinaSyrotaa <Karina.Syrota1@marist.edu>
2022-11-29 18:00:32 -0500

    Upgrade function set_status_visited

--- locale.py
+++ locale.py
@@ -10,10 +10,9 @@ class Locale:
         return "{}".format(self.summary) if self.was_visited == False else "You are at {}".format(self.name)
     
     #Mutator method
-    def set_status_visited(self, status):
-        self.was_visited = status
+    def set_status_visited(self):
+        self.was_visited = True
         
     #Observer method 
     def examine_command(self):
         return self.details
-
